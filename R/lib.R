9662ef7fbb09b19bfbdebafe97a3a214343f354e
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2020-10-01 07:08:18 +0800

    Remove library()

--- R/lib.R
+++ R/lib.R
@@ -1,11 +1,9 @@
-library(tidyverse)
-
 explain_signal <- function(x1) {
-    x0 <- lag(x1)
-    x2 <- lead(x1)
-    if_else(
+    x0 <- dplyr::lag(x1)
+    x2 <- dplyr::lead(x1)
+    dplyr::if_else(
         x1,
-        case_when(
+        dplyr::case_when(
             !x0 ~ "long",
             !x2 ~ "short",
             is.na(x0) ~ "long",
